ed96afb913043f23c4774b38635ca4a36c0624a5
late-bloomer82 <nateriver72@outlook.com>
2023-06-18 16:29:32 -0400

    Created the playRound function that basically contains the whole logic of the rockpaperscissors game and wrote some things that could be improved for the future in the readme.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Rock-Paper-Scissors
 First Js Project from scratch.
+
+
+Lessons learned:
+
+1. Gotta get better and more familliar with arrays and string methods like Math.floor, Math.random, length ,substring, slice, indexOf,charAt etc.
+2. There is most probably a better way of writing those if statements, maybe by using a switch but i cant be bothered to do it right now :P
--- RPS.js
+++ RPS.js
@@ -1,7 +1,8 @@
 //Function that gets the computer to make a random choice between rock papers and scissors
+//Creating an array that contains all possible choices and that can be accessed from anywhere
+const words = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
-    //Creating an array that contains all possible choices
-    const words = ["Rock", "Paper", "Scissors"];
+    
 
     //Creating an index randomizer to choose between the three words
     let randomIndexGenerator = Math.floor(Math.random() * words.length)
@@ -10,5 +11,60 @@ function getComputerChoice() {
     let choice = words[randomIndexGenerator]
     return choice;
 }
+let getPlayerSelection = prompt("Make your choice : ")
 
-console.log(getComputerChoice())
+function playRound(getComputerSelection, getPlayerSelection){
+    getComputerSelection(getComputerChoice);
+    getPlayerSelection = prompt("Make your choice : ")
+    return getPlayerSelection;
+    //There should be 9 different combinations possible.
+
+    //Rock vs Rock
+    if(getPlayerSelection === words[0] && getComputerSelection === words[0]){
+        console.log("Draw, both players chose Rock.")
+
+    }
+    //Paper vs Paper
+    if(getPlayerSelection === words[1] && getComputerSelection === words[1]){
+        console.log("Draw, both players chose Paper.")
+
+    }
+    //Scissors vs Scissors
+    if(getPlayerSelection === words[2] && getComputerSelection === words[2]){
+        console.log("Draw, both players chose Scissors.")
+
+    }
+    //PLayer Rock vs Paper
+    if(getPlayerSelection === words[0] && getComputerSelection === words[1]){
+        console.log("You lose! Computer chose Paper.")
+
+    }
+    // Player Rock vs Scissors
+    if(getPlayerSelection === words[0] && getComputerSelection === words[2]){
+        console.log("You win! Computer chose Scissors.")
+
+    }
+    //Player Paper vs Rock
+    if(getPlayerSelection === words[1] && getComputerSelection === words[0]){
+        console.log("You win! Computer chose Rock")
+
+    }
+    //Player Paper vs Scissors
+    if(getPlayerSelection === words[1] && getComputerSelection === words[2]){
+        console.log("You lose! Computer chose Scissors.")
+
+    }
+    //Player Scissors vs Rock
+    if(getPlayerSelection === words[2] && getComputerSelection === words[0]){
+        console.log("You lose! Computer chose Rock.")
+
+    }
+    //Player Scissors vs Paper
+    if(getPlayerSelection === words[2] && getComputerSelection === words[1]){
+        console.log("You win! Computer chose Paper.")
+
+    }
+    
+    
+}
+

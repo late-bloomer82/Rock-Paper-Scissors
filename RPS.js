8aa8b190f397d29c3ebc4324026c78411dccbda3
late-bloomer82 <nateriver72@outlook.com>
2023-06-18 17:01:59 -0400

    Added a game function to play the game for 5 round with score tracking.

--- RPS.js
+++ RPS.js
@@ -2,7 +2,7 @@
 //Creating an array that contains all possible choices and that can be accessed from anywhere
 const words = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
-    
+
 
     //Creating an index randomizer to choose between the three words
     let randomIndexGenerator = Math.floor(Math.random() * words.length)
@@ -11,60 +11,83 @@ function getComputerChoice() {
     let choice = words[randomIndexGenerator]
     return choice;
 }
-let getPlayerSelection = prompt("Make your choice : ")
 
-function playRound(getComputerSelection, getPlayerSelection){
+ let playerWinCount;
+ let computerWinCount
+
+
+ function playRound(getComputerSelection, getPlayerSelection) {
     getComputerSelection(getComputerChoice);
     getPlayerSelection = prompt("Make your choice : ")
-    return getPlayerSelection;
+
     //There should be 9 different combinations possible.
 
     //Rock vs Rock
-    if(getPlayerSelection === words[0] && getComputerSelection === words[0]){
+    if (getPlayerSelection === words[0] && getComputerSelection === words[0]) {
         console.log("Draw, both players chose Rock.")
 
     }
     //Paper vs Paper
-    if(getPlayerSelection === words[1] && getComputerSelection === words[1]){
+    if (getPlayerSelection === words[1] && getComputerSelection === words[1]) {
         console.log("Draw, both players chose Paper.")
 
     }
     //Scissors vs Scissors
-    if(getPlayerSelection === words[2] && getComputerSelection === words[2]){
+    if (getPlayerSelection === words[2] && getComputerSelection === words[2]) {
         console.log("Draw, both players chose Scissors.")
 
     }
     //PLayer Rock vs Paper
-    if(getPlayerSelection === words[0] && getComputerSelection === words[1]){
+    if (getPlayerSelection === words[0] && getComputerSelection === words[1]) {
         console.log("You lose! Computer chose Paper.")
+        computerWinCount++
 
     }
     // Player Rock vs Scissors
-    if(getPlayerSelection === words[0] && getComputerSelection === words[2]){
+    if (getPlayerSelection === words[0] && getComputerSelection === words[2]) {
         console.log("You win! Computer chose Scissors.")
-
+        playerWinCount++
     }
     //Player Paper vs Rock
-    if(getPlayerSelection === words[1] && getComputerSelection === words[0]){
+    if (getPlayerSelection === words[1] && getComputerSelection === words[0]) {
         console.log("You win! Computer chose Rock")
-
+        playerWinCount++
     }
     //Player Paper vs Scissors
-    if(getPlayerSelection === words[1] && getComputerSelection === words[2]){
+    if (getPlayerSelection === words[1] && getComputerSelection === words[2]) {
         console.log("You lose! Computer chose Scissors.")
-
+        computerWinCount++
     }
     //Player Scissors vs Rock
-    if(getPlayerSelection === words[2] && getComputerSelection === words[0]){
+    if (getPlayerSelection === words[2] && getComputerSelection === words[0]) {
         console.log("You lose! Computer chose Rock.")
-
+        computerWinCount++
     }
     //Player Scissors vs Paper
-    if(getPlayerSelection === words[2] && getComputerSelection === words[1]){
+    if (getPlayerSelection === words[2] && getComputerSelection === words[1]) {
         console.log("You win! Computer chose Paper.")
+        playerWinCount++
 
     }
+
+    console.log(playerWinCount)
+    console.log(computerWinCount)
     
     
+
 }
+
+function game() {
+    for (i = 0; i < 5; i++) {
+        console.log(playRound(getComputerChoice, getPlayerSelection))
+        if(computerWinCount ==5){
+            console.log("Computer wins.")
+
+        }
+        if(p==5){
+            console.log("Player One wins!")
+        }
+    }
+}
+
 

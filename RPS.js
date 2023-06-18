d76237ed984104bc731916efebb390ea84857831
late-bloomer82 <nateriver72@outlook.com>
2023-06-18 14:19:39 -0400

    After a few trials and errors and help from online ressources, Isucessfuly developped a function that randomly selects a word between rock papers and scissors and returns the result.

--- RPS.js
+++ RPS.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+//Function that gets the computer to make a random choice between rock papers and scissors
+function getComputerChoice() {
+    //Creating an array that contains all possible choices
+    const words = ["Rock", "Paper", "Scissors"];
+
+    //Creating an index randomizer to choose between the three words
+    let randomIndexGenerator = Math.floor(Math.random() * words.length)
+
+    //Acessing the random element inside my initial array and returning it
+    let choice = words[randomIndexGenerator]
+    return choice;
+}
+
+console.log(getComputerChoice())
